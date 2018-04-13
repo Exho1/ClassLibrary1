52be2d8f3b6ba233921ae51446c51c68c0ea1b8a
Exho1 <nat.adams149@gmail.com>
2018-04-12 20:46:47 -0500

    Created

---
+++ ClassLibrary1/Uploader.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+
+using Dropbox;
+
+namespace DropboxUpload
+{
+    public class Uploader
+    {
+        public Uploader()
+        {
+
+        }
+
+        public async Task UploadVideoFile(string sourcePath, string newPath)
+        {
+            Console.WriteLine("Starting upload");
+
+            // Create a new dropbox client
+            DropboxClient client = new DropboxClient(Credentials.key, Credentials.secret);
+
+            // Create a request to upload a file
+            UploadFileRequest req = new UploadFileRequest(newPath);
+
+            Console.WriteLine("Request created...");
+
+            // Create a stream which pulls the data from the file on our computer that we want to upload
+            FileStream stream = new FileStream(sourcePath, FileMode.Open);
+
+            Console.WriteLine("Stream created...");
+
+            Console.WriteLine("Uploading file...");
+
+            // Start the upload
+            PutFileResult res = await client.UploadFileAsync(req, stream, Credentials.apiKey);
+
+            Console.WriteLine("Upload finished");
+
+        }
+    }
+}
---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using DropboxUpload;
+
+namespace ConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            MainAsync(args);
+
+            Console.ReadLine();
+        }
+
+        static async void MainAsync(string[] args)
+        {
+            Uploader testUpload = new Uploader();
+            await testUpload.UploadVideoFile(@"E: \Users\Nathaniel\Videos\Sample Videos\Bear.wmv", "ITSABEAR.wmv");
+        }
+    }
+}
