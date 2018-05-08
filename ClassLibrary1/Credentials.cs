f42ba66b561d1e71c76169aa1459b9fe05a308ff
Exho1 <nat.adams149@gmail.com>
2018-05-07 20:49:32 -0500

    creds

---
+++ ClassLibrary1/Credentials.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DropboxUpload
+{
+    /// <summary>
+    /// 
+    /// </summary>
+    public static class Credentials
+    {
+        public static string key = "ys3a897pjxhuiiw";
+        public static string secret = "pv19yfmj3m2cvj9";
+        public static string apiKey = "wqNI3djeoJAAAAAAAAAABcYvA-z0gBP9QDgbMANRUB7MyJNjaMBa_AJExn6oVQB9";
+
+    }
+}
